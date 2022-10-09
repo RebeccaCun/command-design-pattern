5a763993140d1c21ceb1925814b4999678775542
ossendorfluca <luca-rita.ossendorf@stud.uni-bamberg.de>
2022-10-09 16:33:09 -0400

    added Document class with rough structure

---
+++ command/Document.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Document {
+    private String fileName;
+    private ArrayList<String> lines;
+
+    public Document(String fileName){}
+
+    public String view(){}
+
+    public String append(String line){}
+
+    public String write(String line){}
+
+    public String save(){}
+
+}
