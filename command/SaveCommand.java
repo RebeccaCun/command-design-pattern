84f85607991cc82ff063a0b084cb47901a9b2067
RebeccaCun <beckcham@DESKTOP-LIOSI31.localdomain>
2022-10-09 18:26:55 -0400

    commit 2; completing WriteCommand and SaveCommand

--- command/SaveCommand.java
+++ command/SaveCommand.java
@@ -1,12 +1,24 @@
 package command;
 
+/**
+ * A SaveCommand class that is the child of the Command class.
+ * @author Cyber Council
+ */
 public class SaveCommand extends Command {
 
+    /**
+     * Establishes an instance of the SaveCommand class.
+     * @param doc The document that will be saved.
+     */
     public SaveCommand(Document doc) {
         super(doc);
     }
 
+    /**
+     * Executes the save command and creates a string telling the user that the document has been saved.
+     * @return A string representation of the document being saved.
+     */
     public String execute() {
-        return "";
+        return this.document.save();
     }
 }
--- command/WriteCommand.java
+++ command/WriteCommand.java
@@ -2,16 +2,28 @@ package command;
 
 import java.util.Scanner;
 
+/**
+ * A WriteCommand class that is the child of the Command class.
+ * @author Cyber Council
+ */
 public class WriteCommand extends Command {
     private Scanner in;
     
+    /**
+     * Establishes an instance of the WriteCommand class.
+     * @param doc The document that will be written into.
+     */
     public WriteCommand(Document doc) {
         super(doc);
+        in = new Scanner(System.in);
     }
 
+    /**
+     * Executes the write command and creates a string telling the user that the text has been added.
+     * @return A string representation of the document being written into.
+     */
     public String execute() {
         System.out.println("Enter text: ");
-        String line = in.nextLine();
-        return "";
+        return document.write(in.nextLine());
     }
 }
