59a501c77cab062203c10a8a76bf80e99b27a7b6
CharSurry <csurry@email.sc.edu>
2022-10-09 16:11:13 -0400

    adding append

--- command/AppendCommand.java
+++ command/AppendCommand.java
@@ -1,14 +1,24 @@
 package command;
 
 import java.util.Scanner;
-
+/**
+ * appends doc to add new sentences
+ */
 public class AppendCommand extends Command{
     private Scanner in;
-
+    /**
+     * brings the doc down
+     * @param doc that is being edited
+     */
     public AppendCommand(Document doc){
         super(doc);
     }
-    public String execute(){
-        return " ";
+    /**
+     * @return String of whats being added
+     */
+    public String execute() {
+        System.out.println("Enter Text");
+        String line = in.nextLine();
+        return line;
     }
 }
